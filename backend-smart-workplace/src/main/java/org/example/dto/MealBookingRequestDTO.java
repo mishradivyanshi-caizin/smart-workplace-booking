650cc6c3cf0a633a56595744a26eef184b85eefd
mishradivyanshi <mdivyanshi563@gmail.com>
2026-01-18 19:58:51 +0530

    add MealBookingRequestDTO for meal booking API

--- backend-smart-workplace/src/main/java/org/example/dto/MealBookingRequestDTO.java
+++ backend-smart-workplace/src/main/java/org/example/dto/MealBookingRequestDTO.java
@@ -1,4 +1,18 @@
 package org.example.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
 public class MealBookingRequestDTO {
+
+    private List<LocalDate> bookingDates;
+
+    private Double latitude;
+
+    private Double longitude;
 }
